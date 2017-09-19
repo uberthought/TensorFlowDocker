fe00204328ad8ac38400781deea4616243276483
Philip Richardson <prichardson@aumansoftware.com>
2017-09-19 08:44:08 -0400

    added ssh.sh and tweaked sshfs

--- sshd/run2.sh
+++ sshd/run2.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-sudo docker run -d -p 2222:22 --name tfssh uberthought/tensorflowcpussh
+# sudo docker run -d -p 2222:22 --name tfssh uberthought/tensorflowcpussh
+sudo docker run -d -p 80:80 -p 2222:22 --name tfssh uberthought/tensorflowcpussh
---
+++ sshd/ssh.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+ssh root@ec2-52-15-146-194.us-east-2.compute.amazonaws.com -p 2222
+
