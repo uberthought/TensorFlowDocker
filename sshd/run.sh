7976cb4c47c3b4b9da2b1d5d88856828f2a4f40a
Ann Richardson <uberthought@gmail.com>
2018-01-13 17:11:40 -0500

    restart container when host restarts

--- sshd/run.sh
+++ sshd/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -d -p 2222:22 -p 80:80 -p 9000:9000 -p 6006:6006 --name tfssh tfssh
+docker run -d -p 2222:22 -p 80:80 -p 9000:9000 -p 6006:6006 --name tfssh --restart unless-stopped tfssh
