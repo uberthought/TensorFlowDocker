aa3bdd8e10f4f79d2b7b397193432c96a03492ef
Philip Richardson <prichardson@aumansoftware.com>
2018-03-03 12:21:03 -0500

    switch to cuda 7.0

--- setup_nvidia.sh
+++ setup_nvidia.sh
@@ -7,8 +7,8 @@ apt-get upgrade -y
 
 # driver
 
-wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_9.1.85-1_amd64.deb
-dpkg -i cuda-repo-ubuntu1604_9.1.85-1_amd64.deb
+wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_9.0.176-1_amd64.deb
+dpkg -i cuda-repo-ubuntu1604_9.0.176-1_amd64.deb
 apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
 apt-get update
 apt-get install -y cuda
